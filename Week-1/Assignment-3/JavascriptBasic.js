aa70a2c07dd022c65d89c0dac3b13d27f3c72545
Jeremy Peng <pengzhihsuan@gmail.com>
2024-03-26 15:41:36 +0800

    upload completed assignment 3

---
+++ Week-1/Assignment-3/JavascriptBasic.js
@@ -0,0 +1,48 @@
+function countAandB(input) {
+  let count = 0;
+  for (let letter of input) {
+    if (letter === "a" || letter === "b") {
+      count += 1;
+    }
+  }
+  return count;
+}
+function toNumber(input) {
+  const lettersRef = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  // iterate through each letter in input array and convert it to its corresponding number based on lettersRef obj
+  input = input.map((letter) => lettersRef[letter]);
+  return input;
+}
+let input1 = ["a", "b", "c", "a", "c", "a", "c"];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+let input2 = ["e", "d", "c", "d", "e"];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
