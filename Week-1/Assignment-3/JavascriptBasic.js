ae2a43595c94b65f3a88300ad4dd22d0e10a10d3
Jeremy Peng <pengzhihsuan@gmail.com>
2024-04-01 21:59:52 +0800

    change the letterRef method to charCode method

--- Week-1/Assignment-3/JavascriptBasic.js
+++ Week-1/Assignment-3/JavascriptBasic.js
@@ -8,37 +8,12 @@ function countAandB(input) {
   return count;
 }
 function toNumber(input) {
-  const lettersRef = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-    e: 5,
-    f: 6,
-    g: 7,
-    h: 8,
-    i: 9,
-    j: 10,
-    k: 11,
-    l: 12,
-    m: 13,
-    n: 14,
-    o: 15,
-    p: 16,
-    q: 17,
-    r: 18,
-    s: 19,
-    t: 20,
-    u: 21,
-    v: 22,
-    w: 23,
-    x: 24,
-    y: 25,
-    z: 26,
-  };
-  // iterate through each letter in input array and convert it to its corresponding number based on lettersRef obj
-  input = input.map((letter) => lettersRef[letter]);
-  return input;
+  let charCodeArr = [];
+  input = input.join("");
+  for (let i = 0; i < input.length; i++) {
+    charCodeArr.push(input.charCodeAt(i) - 96);
+  }
+  return charCodeArr;
 }
 let input1 = ["a", "b", "c", "a", "c", "a", "c"];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
