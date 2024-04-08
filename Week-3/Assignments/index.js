ea38ad19152fefe826f53816cbce4fe437727f31
Jeremy Peng <pengzhihsuan@gmail.com>
2024-04-08 15:16:48 +0800

    complete assignment 2 requirements

--- Week-3/Assignments/index.js
+++ Week-3/Assignments/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+const AddFromOne = (number) => {
+  return ((1 + number) * number) / 2;
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello My Server!");
+});
+
+app.get("/data", (req, res) => {
+  const queryString = req.query;
+
+  if (Object.keys(queryString).length === 0) {
+    res.status(400).end("Lack of Parameter!");
+  } else if (/[^0-9]/.test(queryString.number)) {
+    res.status(400).end("Wrong Parameter! Please Type In A Number");
+  }
+
+  const result = AddFromOne(queryString.number * 1);
+  res.status(200).json({ result: result });
 });
 
 const server = app.listen(3000, () => {
