2844aa2940fafc14f4e3cf92f33252c17a1e26e7
Jeremy Peng <pengzhihsuan@gmail.com>
2024-04-15 09:22:41 +0800

    Add: complete assigment-1 part B

--- Week-4/Assignment-1/index.js
+++ Week-4/Assignment-1/index.js
@@ -8,3 +8,13 @@ delayedResult(4, 5, 3000, function (result) {
 delayedResult(-5, 10, 2000, function (result) {
   console.log(result);
 }); // 5 (-5+10) will be shown in the console after 2 seconds
+
+function delayedResultPromise(n1, n2, delayTime) {
+  return new Promise((resolve, reject) => {
+    const res = n1 + n2;
+    setTimeout(() => resolve(res), 3000);
+  });
+}
+
+delayedResultPromise(4, 5, 3000).then(console.log);
+// 9 (4+5) will be shown in the console after 3 seconds
