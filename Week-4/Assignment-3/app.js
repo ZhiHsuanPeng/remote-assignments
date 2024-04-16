f243721aa2bc12a52d49ba4a88668eb8bdb67fe3
Jeremy Peng <pengzhihsuan@gmail.com>
2024-04-16 09:27:10 +0800

    Add: finish createUser function

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const db = require("./db");
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 app.get("/home", (req, res) => {
   res.render("login");
@@ -16,7 +18,24 @@ app.get("/signup", (req, res) => {
 });
 
 //TODO:
-app.get("api/v1/users/signup", (req, res) => {});
+app.post("/api/v1/users/signup", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  // 1.) Check if the email already exists
+  const user = await db.getUserByEmail(email);
+
+  if (user) {
+    return res.status(400).json({ message: "User already exists!" });
+  }
+
+  // 2.) Add user to the user table
+
+  const newUser = await db.createUser(email, password);
+
+  // 3.) Send some response
+  res.status(200).json({ newUser: newUser });
+});
 app.get("api/v1/users/login", (req, res) => {});
 
 app.listen(3000, () => {
--- Week-4/Assignment-3/db.js
+++ Week-4/Assignment-3/db.js
@@ -11,17 +11,23 @@ const pool = mysql
   })
   .promise();
 
-const getUser = async () => {
+exports.getUser = async () => {
   const [rows] = await pool.query("SELECT * FROM user");
   console.log(rows);
   return rows;
 };
 
-const createUser = async (email, password) => {
+exports.getUserByEmail = async (email) => {
+  const [rows] = await pool.query("SELECT * FROM user WHERE email = ? ", [
+    email,
+  ]);
+  return rows[0];
+};
+
+exports.createUser = async (email, password) => {
   const [res] = await pool.query(
     "INSERT INTO user (email, password) VALUES (?,?)",
     [email, password]
   );
-  console.log({ id: res.insertId, email, password });
   return { id: res.insertId, email, password };
 };
