399489124650548ed7dad7df21f39303d5aa34cb
Jeremy Peng <pengzhihsuan@gmail.com>
2024-04-17 12:55:21 +0800

    FIX: async function in assignment-1 didn't follow the format provided, change from main (n1, n2, delayTime) to main()

--- Week-4/Assignment-1/index.js
+++ Week-4/Assignment-1/index.js
@@ -1,7 +1,9 @@
+// Callback function
 function delayedResult(n1, n2, delayTime, callback) {
   const res = n1 + n2;
   setTimeout(() => callback(res), delayTime);
 }
+
 delayedResult(4, 5, 3000, function (result) {
   console.log(result);
 }); // 9 (4+5) will be shown in the console after 3 seconds
@@ -9,6 +11,7 @@ delayedResult(-5, 10, 2000, function (result) {
   console.log(result);
 }); // 5 (-5+10) will be shown in the console after 2 seconds
 
+// Promise version
 function delayedResultPromise(n1, n2, delayTime) {
   return new Promise((resolve, reject) => {
     const res = n1 + n2;
@@ -19,9 +22,11 @@ function delayedResultPromise(n1, n2, delayTime) {
 delayedResultPromise(4, 5, 3000).then(console.log);
 // 9 (4+5) will be shown in the console after 3 seconds
 
-async function main(n1, n2, delayTime) {
-  const res = await delayedResultPromise(n1, n2, delayTime);
+// async await version
+async function main() {
+  const res = await delayedResultPromise(4, 5, 3000);
   console.log(res);
   return res;
 }
-main(4, 5, 3000);
+
+main();
